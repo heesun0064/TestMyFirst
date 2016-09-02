52eef9a1f989fe72efd65e4621e4185c2ec8fa36
Heesun Kim <heesun0064@gmail.com>
2016-09-02 12:44:12 +0900

    third line...

--- MyFirstJava/src/MyFirstClass.java
+++ MyFirstJava/src/MyFirstClass.java
@@ -5,5 +5,8 @@ public class MyFirstClass
 	{
 		System.out.println("Hello world !");
 		System.out.println("===============");
+		System.out.println("1. red");
+		System.out.println("2. green");
+		System.out.println("3. blue");
 	}
 }
