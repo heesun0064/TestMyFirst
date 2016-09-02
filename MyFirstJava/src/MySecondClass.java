3a9685616d554e79ff60717c82a8b3e0d7c0cc10
Heesun Kim <heesun0064@gmail.com>
2016-09-02 17:34:40 +0900

    added second class

---
+++ MyFirstJava/src/MySecondClass.java
@@ -0,0 +1,14 @@
+
+public class MySecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1. red");
+		System.out.println("2. green");
+		System.out.println("3. blue");
+		System.out.println("4. white");
+		System.out.println("5. black");
+		System.out.println("6. grey");
+	}
+
+}
