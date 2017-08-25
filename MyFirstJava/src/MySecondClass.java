98467fe4bf516952b33c1f685ec0214ea59d2fc3
Tony <Tony@Tony-PC>
2017-08-25 09:14:30 +0900

    test changes in second class

--- MyFirstJava/src/MySecondClass.java
+++ MyFirstJava/src/MySecondClass.java
@@ -1,8 +1,10 @@
 
-public class MySecondClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class MySecondClass
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world !");
+		System.out.println("===============");
 		System.out.println("1. red");
 		System.out.println("2. green");
 		System.out.println("3. blue");
@@ -12,5 +14,4 @@ public class MySecondClass {
 		System.out.println("7. purple");
 		System.out.println("8. yellow");
 	}
-
 }
